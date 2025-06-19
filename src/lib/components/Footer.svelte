867bcadb8ae9ead94b549e5ee0a4445f4ed2baea
Harsh Sharma <harshsharma90866@gmail.com>
2025-06-19 22:21:52 +0530

    fix footer to bottom

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,7 +2,7 @@
   const currentYear = new Date().getFullYear();
 </script>
 
-<footer class="bg-white border-t border-gray-200">
+<footer class="bg-white border-t border-gray-200 mt-auto w-full">
   <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
     <div class="flex justify-between items-center">
       <div class="text-sm text-gray-500">
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,15 @@
   let { children } = $props();
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
-    import Footer from '$lib/components/Footer.svelte';
-
-
+  import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class = "min-h-screen bg-gray-50">
-    <Header  />
+<div class="min-h-screen bg-gray-50 flex flex-col justify-between">
+  <div>
+    <Header />
     <main>
-        <slot/>
+      <slot/>
     </main>
-    <Footer />
+  </div>
+  <Footer />
 </div>
-<!-- {@render children()} -->
